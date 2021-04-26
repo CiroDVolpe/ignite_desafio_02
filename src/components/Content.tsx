4868864723de977269aa7683436232fac858e0b9
Ciro Della Volpe <cirodvolpe@gmail.com>
2021-04-26 17:36:21 -0300

    Finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
-  // Complete aqui
+import { Header } from './Header';
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+
+interface ContentProps {
+  movies: Movie[];
+  title: string;
+}
+interface Movie{
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <Header title={props.title} />
+      
+      <main>
+        <div className="movies-list">
+          { props.movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
